dfe7dc04331f8aeb58bb56091d0d3fcebb847b77
moises12345654321 <moises.berenguer@gmail.com>
2016-11-14 00:35:51 +0000

    V1.9

--- daily_planet_app/__init__.py
+++ daily_planet_app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 from pymongo import *
  
-app = Flask(__name__, template_folder = 'templates', static_folder = 'static')
+app = Flask(__name__, template_folder = 'templates', static = 'static')
 
 # MongoDB Connection with PyMongo
 def initDB ():
