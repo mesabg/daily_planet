a2f9067207e42c8b28daf45aaa37274a41eeddc4
moises12345654321 <moises.berenguer@gmail.com>
2016-11-16 01:22:13 +0000

    V1.9

--- daily_planet_app/models.py
+++ daily_planet_app/models.py
@@ -9,11 +9,11 @@ class Model:
         
     def getSixFeed(self, inicio):
         save = list(self.db.articulos.aggregate([{ '$sort': {'fecha':1} },{'$project':{ '_id':1, 'imagen':1, 'nombre':1, 'cuerpo':1 }}]))
-        array = list() 
+        #array = list() 
         
-        for i in range(inicio, inicio+6):
-            if len(save) == i:
-                break
-            else:
-                array.append(save[i])
-        return array
+        #for i in range(inicio, inicio+6):
+        #    if len(save) == i:
+        #        break
+        #    else:
+        #        array.append(save[i])
+        return save
