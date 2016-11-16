f3da5bffdd2b30dfd9a721b8b5eb30da09a70c12
moises12345654321 <moises.berenguer@gmail.com>
2016-11-16 00:54:04 +0000

    V1.9

--- daily_planet_app/models.py
+++ daily_planet_app/models.py
@@ -6,13 +6,12 @@ class Model:
         self.client = MongoClient()
         self.db = self.client.daily_planet_db
         
-    def getSixFeed(inicio):
-        save = self.db.articulos.aggregate([{ $sort: {"fecha":1} },{$project:{ _id:1, imagen:1, nombre:1, cuerpo:1 }}]).toArray()
+    def getSixFeed(self, inicio):
+        save = self.db.articulos.aggregate([{ "$sort": {"fecha":1} },{"$project":{ _id:1, imagen:1, nombre:1, cuerpo:1 }}]).toArray()
         array = []
         for i in range(inicio, inicio+6):
             if save.length == i:
                 break
-            else
+            else:
                 array += save[i]
-            
         return array
