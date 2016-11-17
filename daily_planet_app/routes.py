8341c81429947a3219453553042ed23ad7952710
moises12345654321 <moises.berenguer@gmail.com>
2016-11-17 03:34:27 +0000

    V12

--- daily_planet_app/routes.py
+++ daily_planet_app/routes.py
@@ -113,8 +113,8 @@ def create_routes(app, model):
     def upload_comentario():
         id_usuario = int(request.args.get('id_usuario'))
         id_articulo = int(float(request.args.get('id_articulo')))
-        text = request.args.get('comentario_cuerpo')
-        #text = request.form['comentario_cuerpo']
+        #text = request.args.get('comentario_cuerpo')
+        text = request.form['comentario_cuerpo']
         comentario_nuevo = model.upload_comentario(id_articulo, id_usuario, text)
         print(id_usuario, id_articulo, text)
         return Response(comentario_nuevo, status=200, headers=None, mimetype='application/json')
