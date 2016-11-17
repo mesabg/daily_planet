5b3a2fbd56c4bf15d88e2cbbc12d2f1a38ccd504
moises12345654321 <moises.berenguer@gmail.com>
2016-11-17 03:50:41 +0000

    V12

--- daily_planet_app/routes.py
+++ daily_planet_app/routes.py
@@ -115,5 +115,5 @@ def create_routes(app, model):
         id_articulo = int(float(request.args.get('id_articulo')))
         #text = request.args.get('comentario_cuerpo')
         text = request.form['comentario_cuerpo']
-        data = model.upload_comentario(id_articulo, id_usuario, text)
+        data = json.dumps(model.upload_comentario(id_articulo, id_usuario, text))
         return Response(data, status=200, headers=None, mimetype='application/json')
