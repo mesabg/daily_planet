bdefbce94b9e72a93ad3b471c7b1d0eac848ea3e
moises12345654321 <moises.berenguer@gmail.com>
2016-11-17 15:12:53 +0000

    V12

--- daily_planet_app/static/js/articulos_no_publicados.js
+++ daily_planet_app/static/js/articulos_no_publicados.js
@@ -19,7 +19,7 @@ function load_more_nopub(){
     .done(function(data) {
         /*Render*/
         for(var i=0; i<data.length;i++){
-            var tr = $('<tr><td class="Nombre">'+data[i].nombre+'</td> <td class="Fecha">'+data[i].fecha+'</td> <td><center>'+data[i].editando+'</center></td> <td><a href="/modificar_articulo?id='+data[i]._id+'&nombre='+data[i].nombre+'&resumen='+data[i].resumen+'&palabras='+data[i].palabras+'&cuerpo='+data[i].cuerpo+'&imagen='+data[i].imagen+'" class="btn btn-warning col-md-6">Modificar</a><button class="btn btn-success col-md-6" onclick="publicar(this.parentNode.parentNode)">Publicar</button></td></tr>')
+            var tr = $('<tr><td class="Nombre">'+data[i].nombre+'</td> <td class="Fecha">'+data[i].fecha+'</td> <td><center>'+data[i].editando+'</center></td> <td><a href="/modificar_articulo?id='+data[i]._id+'" class="btn btn-warning col-md-6">Modificar</a><button class="btn btn-success col-md-6" onclick="publicar(this.parentNode.parentNode)">Publicar</button></td></tr>')
            // var div = $('<div class="col-md-4 banner-bottom-grid"> <a href="/single?id='+data[i]._id+'" > <img src="/get_image?path='+data[i].imagen+'"/>  </a><h4>'+data[i].nombre+'</h4> <p>'+data[i].resumen+'</p> </div>');
             $("#table").append(tr);
         }
