1c6a9078bdf10cd8320429dee07efdf6581afa7a
moises12345654321 <moises.berenguer@gmail.com>
2016-11-18 03:35:18 +0000

    V12

--- daily_planet_app/models.py
+++ daily_planet_app/models.py
@@ -129,7 +129,7 @@ class Model:
         
         
     def addfav(self,_id_art,_id):
-        array = self.db.articulos.find_one({'_id':_id_art},{'favoritos':1,'_id':0})
+        array = self.db.articulos.find_one({'_id':_id_art},{'favoritos':1,'_id':0})['favoritos']
         
         print("-------------------------------------------------------")
         print(_id_art)
